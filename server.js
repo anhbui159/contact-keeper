13fc2cd9d13f42a1fa310c451c12575fcf22b25c
anhbui159 <33772016+anhbui159@users.noreply.github.com>
2021-06-13 21:32:22 -0400

    setup mongoose with user schema
    setup mongoose with user schema

--- server.js
+++ server.js
@@ -1,13 +1,16 @@
 const express = require('express');
+const connectDB = require('./config/db');
 
 const app = express();
 
-// Define routes
+//Connect Database
+connectDB();
 
+// Define routes
 app.use('/api/users', require('./routes/users'));
 app.use('/api/auth', require('./routes/auth'));
-app.use('/api/contacts', require('.routes/contacts'));
+app.use('/api/contacts', require('./routes/contacts'));
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => console.log(`Server started on port ${PORT}`););
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
