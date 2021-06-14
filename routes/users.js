6df088e7b6027779cb47da2c20596e916a53ce6e
anhbui159 <33772016+anhbui159@users.noreply.github.com>
2021-06-14 16:37:05 -0400

    Auth
    Added authentication using JWT

--- routes/users.js
+++ routes/users.js
@@ -1,12 +1,74 @@
 const express = require('express');
-
 router = express.Router();
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const config = require('config');
+const { check, validationResult } = require('express-validator');
+
+const User = require('../models/User');
 
 // @route POST api/users
 // @desc Register a user
 // @access Public
-router.post('/', (req, res) => {
-  res.send('Register an user');
-});
+router.post(
+  '/',
+  [
+    check('name', 'Please add name').not().isEmpty(),
+    check('email', 'Please include a valid email').isEmail(),
+    check(
+      'password',
+      'Please enter a password with 6 or more characters'
+    ).isLength({ min: 6 }),
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    const { name, email, password } = req.body;
+
+    try {
+      //Check if that email has already been used
+      let user = await User.findOne({ email });
+      if (user) {
+        return res.status(400).json({ msg: 'User already exists' });
+      }
+      //Creating a new user
+      user = new User({
+        name,
+        email,
+        password,
+      });
+      //Generate salt
+      const salt = await bcrypt.genSalt(10);
+      //Hashing the plain password before storing
+      user.password = await bcrypt.hash(password, salt);
+      //Save the user to database
+      await user.save();
+
+      const payload = {
+        user: {
+          id: user.id,
+        },
+      };
+
+      jwt.sign(
+        payload,
+        config.get('jwtSecret'),
+        {
+          expiresIn: 360000,
+        },
+        (err, token) => {
+          if (err) throw err;
+          res.json({ token });
+        }
+      );
+    } catch (err) {
+      console.error(err.message);
+      res.status(500).send('Server Error');
+    }
+  }
+);
 
 module.exports = router;
--- server.js
+++ server.js
@@ -6,6 +6,9 @@ const app = express();
 //Connect Database
 connectDB();
 
+//Init middleware
+app.use(express.json({ extended: false }));
+
 // Define routes
 app.use('/api/users', require('./routes/users'));
 app.use('/api/auth', require('./routes/auth'));
